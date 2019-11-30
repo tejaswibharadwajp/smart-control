3665191a3541ad8c65797784fc8624fe6e46a03e
Teja Pullabhotla <teja.pullabhotla@am.jll.com>
2019-11-30 12:16:40 -0600

    master: Added user authentication.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import logo from './logo.svg';
-
+import {Button, Grid, Paper, TextField} from '@material-ui/core';
 import './App.css';
+
 require('dotenv').config();
 
 
@@ -13,23 +13,38 @@ class App extends React.Component {
         super(props);
         this.state = {
             kasaAccProps: "",
-            kasaDevicesList: ""
-        }
+            kasaDevicesList: "",
+            isSignedIn: false,
+            showError: false,
+        };
         this.controlSwitch = this.controlSwitch.bind(this);
+        this.controlBulb = this.controlBulb.bind(this);
+        this.handleInputChange = this.handleInputChange.bind(this);
     }
 
-    async componentDidMount() {
+    handleChange = name => event => {
+        this.setState({[name]: event.target.value});
+    };
+
+    async handleInputChange() {
+        const {username, password} = this.state;
         try {
-            let username = process.env.KASA_USER_NAME;
-            let pass = process.env.KASA_PASSWORD;
-
-            const tpLink = await login(username, pass, "");
-            console.log(tpLink);
-            this.setState({kasaAccProps: tpLink});
-            let deviceList = await tpLink.getDeviceList();
-            console.log(deviceList);
+            if(username && password){
+                const tpLink = await login(username, password, "");
+                if (tpLink && tpLink.error_code) {
+                    this.setState({isSignedIn: false, showError: true});
+                } else {
+                    this.setState({isSignedIn: true, showError: false});
+                    this.setState({kasaAccProps: tpLink});
+                    let deviceList = await tpLink.getDeviceList();
+                    console.log(deviceList);
+                }
+            }
+            else{
+                this.setState({isSignedIn: false, showError: true});
+            }
         } catch (e) {
-
+            console.log(e)
         }
     }
 
@@ -46,21 +61,75 @@ class App extends React.Component {
         }
     }
 
+    async controlBulb(on) {
+        try {
+            const {kasaAccProps} = this.state;
+            if (on) {
+                await kasaAccProps.getLB130("Closet Light").setState(1, 80, 300, 100);
+            } else {
+                await kasaAccProps.getLB130("Closet Light").powerOff();
+            }
+        } catch (e) {
+
+            console.log(e)
+        }
+    }
 
     render() {
+        const {isSignedIn, showError} = this.state;
         return (
-            <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo"/>
-                    <button onClick={() => {
-                        this.controlSwitch(true)
-                    }}> Turn On
-                    </button>
-                    <button onClick={() => {
-                        this.controlSwitch(false)
-                    }}> Turn Off
-                    </button>
-                </header>
+            <div>
+                <Grid item xs={12} sm={3}>
+                    {!isSignedIn && <Paper>
+                          <div>
+                            <TextField
+                                id="username"
+                                label="Username"
+                                type="text"
+                                margin="normal"
+                                onChange={this.handleChange('username')}
+                            />
+                        </div>
+                        <div>
+                            <TextField
+                                id="password"
+                                label="Password"
+                                type="password"
+                                margin="normal"
+                                onChange={this.handleChange('password')}
+                            />
+                        </div>
+                        {showError && <div className='App_errorMessage'> Incorrect Username or Password</div>}
+                        <Button variant="outlined" onClick={() => {
+                            this.handleInputChange();
+                        }}> Submit
+                        </Button>
+                    </Paper>}
+                    {isSignedIn && <Paper>
+                        <div>
+                            Control Smart Plug
+                            <Button variant="outlined" onClick={() => {
+                                this.controlSwitch(true)
+                            }}> Turn On
+                            </Button>
+                            <Button variant="outlined" onClick={() => {
+                                this.controlSwitch(false)
+                            }}> Turn Off
+                            </Button>
+                        </div>
+                        <div>
+                            Control closet bulb
+                            <Button variant="outlined" onClick={() => {
+                                this.controlBulb(true)
+                            }}> Turn On
+                            </Button>
+                            <Button variant="outlined" onClick={() => {
+                                this.controlBulb(false)
+                            }}> Turn Off
+                            </Button>
+                        </div>
+                    </Paper>}
+                </Grid>
             </div>
         );
     }
